9c89b578718d4b0297c9cb10e35d826058f15793
Saba Ahmad <sabaahmad895@gmail.com>
2022-10-11 13:04:41 +0500

    some changes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function ResponsiveMenuForMobile(){
-   let x = document.getElementById('my-mobile-head');
-   let y = document.getElementById('my-header-items');
+   let mobilehead = document.getElementById('my-mobile-head');
+   let items = document.getElementById('my-header-items');
    let z1 = document.getElementById('my-header1');
    let z2 = document.getElementById('my-header2');
    let z3 = document.getElementById('my-header3');
    let B = document.getElementById('my-bd');
 
    if(x.className === 'mobile-header' && y.className === 'header-items'){
-    x.className = 'mobile-headerr';
-    y.className = 'header-itemsr';
+    mobilehead.className = 'mobile-headerr';
+    items.className = 'header-itemsr';
     z1.className = 'headerr';
     z2.className = 'headerr';
     z3.className = 'headerr';
     B.className = 'bdr';
    }
    else{
-    x.className = 'mobile-header';
-    y.className = 'header-items';
+    mobilehead.className = 'mobile-header';
+    items.className = 'header-items';
     z1.className = 'header';
     z2.className = 'header';
     z3.className = 'header';
@@ -25,15 +25,15 @@ function ResponsiveMenuForMobile(){
 }
 
 function OptionClick(){
-   let x = document.getElementById('my-mobile-head');
-   let y = document.getElementById('my-header-items');
+   let mobilehead = document.getElementById('my-mobile-head');
+   let items = document.getElementById('my-header-items');
    let z1 = document.getElementById('my-header1');
    let z2 = document.getElementById('my-header2');
    let z3 = document.getElementById('my-header3');
    let B = document.getElementById('my-bd');
    
-      x.className = 'mobile-header';
-      y.className = 'header-items';
+      mobilehead.className = 'mobile-header';
+      items.className = 'header-items';
       z1.className = 'header';
       z2.className = 'header';
       z3.className = 'header';
